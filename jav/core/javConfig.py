6f1f4678e69cb0b8425aaa159b65ee53ff2a6ee1
Francois G <f.gerthoffert@gmail.com>
2017-05-27 10:59:42 -0400

    Updated code style

--- jav/core/javConfig.py
+++ jav/core/javConfig.py
@@ -4,6 +4,7 @@ import os
 import json
 import jsonschema
 
+
 class Config(object):
     ''' 
         Class in charge to configuration management
@@ -16,9 +17,9 @@ class Config(object):
         self.configFilepath = self.configPath + self.configFilename
         self.currentConfig = None
 
-        if os.path.isdir(self.configPath) == False or os.path.isfile(self.configFilepath) == False:
+        if os.path.isdir(self.configPath) is False or os.path.isfile(self.configFilepath) is False:
             self.initConfig()
-        elif os.path.isfile(self.configFilepath) == True:
+        elif os.path.isfile(self.configFilepath):
             self.currentConfig = self.importConfig()
 
         self.schema = {
@@ -28,16 +29,52 @@ class Config(object):
             , 'type': 'object'
             , 'additionalProperties': True
             , 'properties': {
-                'history_weeks': {'type': ['number'], 'description': 'Number of weeks of history to process from current date', 'default': 12}
-                , 'cache_filepath': {'type': ['string'], 'description': 'Path of the cache file', 'default': self.configPath + 'data.jsonl'}
-                , 'jira_username': {'type': ['string', 'null'], 'description': 'JIRA Username used for the API call'}
-                , 'jira_password': {'type': ['string', 'null'], 'description': 'JIRA Password used for the API call'}
-                , 'jira_host': {'type': ['string', 'null'], 'description': 'Remote JIRA Hostname (https://jira.yourdomain.tld)'}
-                , 'jira_jql_velocity': {'type': ['string', 'null'], 'description': 'JIRA JQL Query used to track story points completion (for example: project = WORK and status changed to Done)'}
-                , 'jira_jql_remaining': {'type': ['string', 'null'], 'description': 'JIRA JQL Query used to determine remaining work (for example: sprint in openSprints() and type in (Story, Task, Defect))'}
-                , 'jira_field_points': {'type': ['string', 'null'], 'description': 'Field used by JIRA to store Story Points', 'default': 'customfield_10002'}
-                , 'slack_channel': {'type': ['string', 'null'], 'description': 'Slack channel to post this message to', 'default': '#general'}
-                , 'slack_webhook': {'type': ['string', 'null'], 'description': 'Slack webhook (https://api.slack.com/incoming-webhooks)'}
+                'history_weeks': {
+                    'type': ['number']
+                    , 'description': 'Number of weeks of history to process from current date'
+                    , 'default': 12
+                }
+                , 'cache_filepath': {
+                    'type': ['string']
+                    , 'description': 'Path of the cache file'
+                    , 'default': self.configPath + 'data.jsonl'
+                }
+                , 'jira_username': {
+                    'type': ['string', 'null']
+                    , 'description': 'JIRA Username used for the API call'
+                }
+                , 'jira_password': {
+                    'type': ['string', 'null']
+                    , 'description': 'JIRA Password used for the API call'
+                }
+                , 'jira_host': {
+                    'type': ['string', 'null']
+                    , 'description': 'Remote JIRA Hostname (https://jira.yourdomain.tld)'
+                }
+                , 'jira_jql_velocity': {
+                    'type': ['string', 'null']
+                    , 'description': 'JIRA JQL Query used to track story points completion '
+                                     '(for example: project = WORK and status changed to Done)'
+                }
+                , 'jira_jql_remaining': {
+                    'type': ['string', 'null']
+                    , 'description': 'JIRA JQL Query used to determine remaining work '
+                                     '(for example: sprint in openSprints() and type in (Story, Task, Defect))'
+                }
+                , 'jira_field_points': {
+                    'type': ['string', 'null']
+                    , 'description': 'Field used by JIRA to store Story Points'
+                    , 'default': 'customfield_10002'
+                }
+                , 'slack_channel': {
+                    'type': ['string', 'null']
+                    , 'description': 'Slack channel to post this message to'
+                    , 'default': '#general'
+                }
+                , 'slack_webhook': {
+                    'type': ['string', 'null']
+                    , 'description': 'Slack webhook (https://api.slack.com/incoming-webhooks)'
+                }
             }
         }
 
@@ -50,20 +87,19 @@ class Config(object):
 
     def getDefaultConfig(self):
         defaultConfig = {
-            'history_weeks' : 12
+            'history_weeks': 12
             , 'cache_filepath': self.configPath + 'data.jsonl'
-            , 'jira_username' : None
-            , 'jira_password' : None
-            , 'jira_host' : None
-            , 'jira_jql_velocity' : None
-            , 'jira_jql_remaining' : None
+            , 'jira_username': None
+            , 'jira_password': None
+            , 'jira_host': None
+            , 'jira_jql_velocity': None
+            , 'jira_jql_remaining': None
             , 'jira_field_points': 'customfield_10002'
-            , 'slack_channel' : None
-            , 'slack_webhook' : None
+            , 'slack_channel': None
+            , 'slack_webhook': None
         }
 
         return defaultConfig
-
 
     def initConfig(self):
         self.log.info('Config.initConfig(): Unable to find config file, initializing')
@@ -72,7 +108,7 @@ class Config(object):
     def writeConfig(self, config):
         self.log.info('Config.writeConfig(): Writing config file')
         self.log.info('Config Object: ' + json.dumps(config))
-        if os.path.isdir(self.configPath) == False:
+        if not os.path.isdir(self.configPath):
             os.mkdir(self.configPath)
 
         configFile = open(self.configFilepath, 'w')
@@ -81,7 +117,7 @@ class Config(object):
         return config
 
     def getConfig(self, key):
-        if self.currentConfig == None:
+        if self.currentConfig is None:
             self.currentConfig = self.importConfig()
 
         jsonschema.validate(self.currentConfig, self.schema)
@@ -99,4 +135,3 @@ class Config(object):
             cfg = yaml.safe_load(ymlfile)
 
         return cfg
-
