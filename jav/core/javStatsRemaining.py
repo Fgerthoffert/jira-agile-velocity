1283dd9920e5acafce44c923e2af4e7ffecd93c9
Francois G <f.gerthoffert@gmail.com>
2017-06-04 10:57:02 -0400

    Corrected bug with writing remaining stats to file

--- jav/core/javStatsRemaining.py
+++ jav/core/javStatsRemaining.py
@@ -30,6 +30,7 @@ class StatsRemaining(object):
                 current_stats_line = json.loads(line)
                 current_stats_line['datetime'] = dateutil.parser.parse(current_stats_line['datetime'])
                 dict_idx = current_stats_line['datetime'].strftime('%Y%m%d')
+                self.log.info('StatsRemaining.load_stat_file(): Loaded stats from: ' + current_stats_line['datetime'].isoformat())
                 daily_data[dict_idx] = current_stats_line
         else:
             self.log.info('StatsRemaining.load_stat_file(): Nothing to load, cache file does not exist')
@@ -52,7 +53,7 @@ class StatsRemaining(object):
 
         remaining = {
             'points': self.__remaining_work['points']
-            , 'datetime': date_current.isoformat()
+            , 'datetime': date_current
             , 'days_to_completion': {}
         }
         self.log.info('Remaining number of story points: ' + str(remaining['points']))
@@ -75,17 +76,19 @@ class StatsRemaining(object):
         daily_data[dict_idx] = remaining
 
         current_data = self.load_stat_file()
+
         for current_day_data in current_data:
             if date_current.strftime('%Y-%m-%d') != current_data[current_day_data]['datetime'].strftime('%Y-%m-%d'):
-                dict_idx = current_day_data[current_day_data]['datetime'].strftime('%Y-%m-%d')
-                daily_data[dict_idx] = current_day_data[current_day_data]
+                dict_idx = current_data[current_day_data]['datetime'].strftime('%Y-%m-%d')
+                daily_data[dict_idx] = current_data[current_day_data]
 
         if os.path.isfile(self.daystats_filepath):
             os.remove(self.daystats_filepath)
 
         for currentdata in daily_data:
             daily_obj = copy.deepcopy(daily_data[currentdata])
-            daily_obj['datetime'] = daily_data[currentdata]['datetime']
+            daily_obj['datetime'] = daily_data[currentdata]['datetime'].isoformat()
+            self.log.info('StatsRemaining.main(): Writing stats to: ' + daily_obj['datetime'])
             self.write_json(daily_obj)
 
         return remaining
