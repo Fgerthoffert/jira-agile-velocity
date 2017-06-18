fdd8db0c452a35510501b0ed2e26ee9697ba74a0
Francois G <f.gerthoffert@gmail.com>
2017-06-18 11:37:21 -0400

    Added first test for config class

---
+++ tests/core/test_Config.py
@@ -0,0 +1,48 @@
+from unittest import TestCase
+import mock
+from jav.core.javConfig import Config
+from cement.core import foundation
+
+class TestConfig(TestCase):
+
+    @classmethod
+    def get_app(self):
+        """App init, necessary to get to the logging service"""
+        app = foundation.CementApp('myapp')
+        app.setup()
+        app.run()
+        return app
+
+    @classmethod
+    def get_config(self):
+        config = {
+            'end_date': '2017-02-01'
+            , 'git_branch': 'gh-pages'
+            , 'git_localpath': '/Users/USERNAME/Desktop/test-agile-page/'
+            , 'git_pageurl': 'https://org.github.io/repo/dir/'
+            , 'git_pathdirectory': 'dir/'
+            , 'git_repo': 'https://github.com/org/repo.git'
+            , 'jira_field_points': 'customfield_10002'
+            , 'jira_host': 'https://URL'
+            , 'jira_jql_remaining': 'JQL QUERY'
+            , 'jira_jql_velocity': 'JQL QUERY'
+            , 'jira_password': 'PASSWORD'
+            , 'jira_username': 'USERNAME'
+            , 'rolling_stats': [4, 8, 12]
+            , 'slack_channel': '#general'
+            , 'slack_webhook': 'SLACK WEBHOOK'
+            , 'stats_metric': 'tickets'
+        }
+        return config
+
+    def test_get_config_value(self):
+        # App init, necessary to get to the logging service
+        app = self.get_app()
+
+        config = Config(app.log)
+        config.config = self.get_config()
+
+        self.assertEqual(config.get_config_value('jira_jql_remaining'), 'JQL QUERY')
+
+
+
