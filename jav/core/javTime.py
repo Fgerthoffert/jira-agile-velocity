1c960f9493e4ff043b4db33964697671132d3f7b
Francois G <f.gerthoffert@gmail.com>
2017-06-18 10:15:00 -0400

    Removed unused function, and add test

--- jav/core/javTime.py
+++ jav/core/javTime.py
@@ -15,28 +15,6 @@ class Time(object):
     def get_current_date():
         return datetime.now(pytz.timezone('America/Toronto'))
 
-    def get_end_date_from_weeks(self):
-        """Search for the oldest Monday past number of history_weeks"""
-        history_weeks = self.config.get_config_value('history_weeks')
-        self.log.info('Time.get_end_date(): Looking back: ' + str(history_weeks) + ' weeks')
-
-        tentative_date = self.get_current_date() - timedelta(weeks=history_weeks)
-        self.log.info('Time.get_end_date(): Tentative date is: ' + tentative_date.isoformat())
-        if tentative_date.strftime('%A') == 'Monday':
-            end_date = tentative_date
-        else:
-            self.log.info('Time.get_end_date(): This date is a: ' + tentative_date.strftime(
-                '%A') + ' ... looking for previous Monday')
-            while 1:
-                tentative_date = tentative_date - timedelta(days=1)
-                if tentative_date.strftime('%A') == 'Monday':
-                    end_date = tentative_date
-                    break
-
-        self.log.info('Time.get_end_date(): End date is: ' + tentative_date.isoformat())
-
-        return end_date
-
     def get_end_date(self):
         """Get end_date as a datetime object from configuration"""
         return datetime.strptime(self.config.get_config_value('end_date'), "%Y-%m-%d").date()
--- tests/core/test_Time.py
+++ tests/core/test_Time.py
@@ -17,11 +17,23 @@ class TestTime(TestCase):
         return app
 
     def test_get_current_date(self):
-        # App init, necessary to get to the logging service
         answer_date = datetime.now(pytz.timezone('America/Toronto')).strftime('%Y-%m-%d')
         response_date = Time.get_current_date().strftime('%Y-%m-%d')
         self.assertEqual(answer_date, response_date)
 
+    @mock.patch('jav.core.javConfig')
+    def test_get_end_date(self, mock_config):
+        mock_config.get_config_value = mock.MagicMock(return_value='2017-01-22')
+
+        # App init, necessary to get to the logging service
+        app = self.get_app()
+
+        time = Time(app.log, mock_config)
+
+        end_date_answer = datetime.strptime('2017-01-22', "%Y-%m-%d").date()
+        end_date_response = time.get_end_date()
+
+        self.assertEqual(end_date_answer, end_date_response)
 
 
 
