05ab203496800c67b4f351647089de4034816de4
Francois Gerthoffert <f.gerthoffert@gmail.com>
2019-09-10 11:49:12 -0400

    Started to add auth to the API

--- api/src/app.controller.ts
+++ api/src/app.controller.ts
@@ -1,9 +1,18 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+import { Request, Post, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+
+@Controller('')
 export class AppController {
   constructor(private readonly appService: AppService) {}
+
+  @UseGuards(AuthGuard('local'))
+  @Post('login')
+  async login(@Request() req) {
+    return req.user;
+  }
 
   @Get()
   getHello(): string {
--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -4,9 +4,11 @@ import { AppService } from './app.service';
 import { VelocityModule } from './velocity/velocity.module';
 import { RoadmapModule } from './roadmap/roadmap.module';
 import { ConfigModule } from './config.module';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [VelocityModule, RoadmapModule, ConfigModule],
+  imports: [VelocityModule, RoadmapModule, ConfigModule, AuthModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
