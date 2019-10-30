6dc84083771591a876b21dd41e6e429ae66d0000
Francois Gerthoffert <f.gerthoffert@gmail.com>
2019-10-30 15:19:36 -0400

    Reduced the velocity payload

--- ui/src/views/velocity/dashboard/WeeklyChart.tsx
+++ ui/src/views/velocity/dashboard/WeeklyChart.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import parseISO from 'date-fns/parseISO';
 import VelocityChartStacked from '../../../components/Charts/ChartJS/VelocityChartStacked';
 
 export interface WeeklyChartsProps {
@@ -25,44 +24,8 @@ export default function WeeklyChart(props: WeeklyChartsProps) {
       };
     });
 
-  const days = [
-    'Sunday',
-    'Monday',
-    'Tuesday',
-    'Wednesday',
-    'Thursday',
-    'Friday',
-    'Saturday'
-  ];
   const datasetWithDays = dataset.map((week: any) => {
-    const weekDays = [];
-    // @ts-ignore
-    for (const [idx, day] of days.entries()) {
-      const issuesList = week.completion.list.filter((issue: any) => {
-        //          console.log(issue);
-        const issueDate = parseISO(issue.closedAt);
-        if (issueDate.getDay() === idx) {
-          return true;
-        }
-        return false;
-      });
-      weekDays.push({
-        weekdayTxt: day,
-        list: issuesList,
-        date: issuesList.length === 0 ? null : parseISO(issuesList[0].closedAt),
-        jql: issuesList.length === 0 ? null : issuesList[0].jql,
-        completion: {
-          issues: {
-            count: issuesList.length
-          },
-          points: {
-            count: issuesList
-              .map((i: any) => i.points)
-              .reduce((acc: number, count: number) => acc + count, 0)
-          }
-        }
-      });
-    }
+    const weekDays = velocity.days.filter((d: any) => d.weekTxt === week.weekTxt);
     return { ...week, weekDays };
   });
   return (
