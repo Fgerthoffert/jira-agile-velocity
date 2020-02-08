926423b8799ca71ba26bb5ca1a18bd6c7fdbc0fb
Francois Gerthoffert <f.gerthoffert@gmail.com>
2020-02-07 22:25:14 -0500

    Modified legends in the initiatives view

--- ui/src/components/Charts/Nivo/RoadmapFutureChart.tsx
+++ ui/src/components/Charts/Nivo/RoadmapFutureChart.tsx
@@ -3,208 +3,221 @@ import { Theme, createStyles, withStyles } from '@material-ui/core/styles';
 import { ResponsiveHeatMap } from '@nivo/heatmap';
 import toMaterialStyle from 'material-color-hash';
 
+import moment from 'moment';
+
 import { getInitiativeTitle } from './utils';
 
 const styles = (theme: Theme) =>
-  createStyles({
-    root: {
-      height: 800,
-    },
-  });
+	createStyles({
+		root: {
+			height: 800
+		}
+	});
 
 interface IDataset {
-  label: string;
-  backgroundColor: string;
-  completed: number[];
-  data: number[];
+	label: string;
+	backgroundColor: string;
+	completed: number[];
+	data: number[];
 }
 interface IDatasetObj {
-  [key: string]: any;
+	[key: string]: any;
 }
 
 interface ICompletionObj {
-  [key: string]: any;
+	[key: string]: any;
 }
 
 class RoadmapFutureChart extends Component<any, any> {
-  completionWeeks: any = {};
+	completionWeeks: any = {};
 
-  /* 
+	/* 
     Display different background colors based on the percentage of the effort spent on a particular activity for a week
   */
-  getCompletionColor = (data: any, value: any) => {
-    const { roadmap } = this.props;
-    const initiative = roadmap.futureCompletion.find(
-      (i: any) => getInitiativeTitle(i) === data.yKey,
-    );
-    if (initiative !== undefined) {
-      return toMaterialStyle(initiative.team.team, 200).backgroundColor;
-    }
-    return 'rgb(65, 171, 93)';
-  };
+	getCompletionColor = (data: any, value: any) => {
+		const { roadmap } = this.props;
+		const initiative = roadmap.futureCompletion.find((i: any) => getInitiativeTitle(i) === data.yKey);
+		if (initiative !== undefined) {
+			return toMaterialStyle(initiative.team.team, 200).backgroundColor;
+		}
+		return 'rgb(65, 171, 93)';
+	};
 
-  cellClick = (initiative: string, weekTxt: string) => {
-    const { roadmap } = this.props;
-    const cellData = roadmap.futureCompletion
-      .find((i: any) => getInitiativeTitle(i) === initiative)
-      .weeks.find((w: any) => w.weekTxt === weekTxt).list;
-    if (cellData.length > 0) {
-      const keys = cellData.map((i: any) => i.key);
-      const url =
-        roadmap.host + '/issues/?jql=key in (' + keys.toString() + ')';
-      window.open(url, '_blank');
-    }
-  };
+	getDayWeek = (week: string) => {
+		const data = week.split('.');
+		const date = moment().isoWeekYear(parseInt(data[0], 10)).isoWeek(parseInt(data[1], 10)).endOf('week');
+		return date.format('MMM Do');
+	};
 
-  buildDataset = (initiatives: any) => {
-    const { defaultPoints } = this.props;
-    let metric = 'points';
-    if (!defaultPoints) {
-      metric = 'issues';
-    }
+	clickLegend = (key: string) => {
+		const { roadmap } = this.props;
+		const initiative = roadmap.initiatives.find((i: any) => getInitiativeTitle(i) === key);
+		if (initiative !== undefined) {
+			const url = roadmap.host + '/browse/' + initiative.key;
+			window.open(url, '_blank');
+		}
+	};
 
-    const dataset: IDatasetObj[] = [];
-    for (const initiative of initiatives) {
-      const initiativeData: IDatasetObj = {
-        initiative: getInitiativeTitle(initiative),
-      };
-      for (const week of initiative.weeks) {
-        initiativeData[week.weekTxt] = week[metric].count;
-      }
-      dataset.push(initiativeData);
-    }
-    return dataset;
-  };
+	cellClick = (initiative: string, weekTxt: string) => {
+		const { roadmap } = this.props;
+		const cellData = roadmap.futureCompletion
+			.find((i: any) => getInitiativeTitle(i) === initiative)
+			.weeks.find((w: any) => w.weekTxt === weekTxt).list;
+		if (cellData.length > 0) {
+			const keys = cellData.map((i: any) => i.key);
+			const url = roadmap.host + '/issues/?jql=key in (' + keys.toString() + ')';
+			window.open(url, '_blank');
+		}
+	};
 
-  render() {
-    const { roadmap, defaultPoints } = this.props;
-    let metric = 'points';
-    if (!defaultPoints) {
-      metric = 'issues';
-    }
-    //console.log(roadmap.futureCompletion);
-    const initiatives = roadmap.futureCompletion.filter(
-      (i: any) => i.metrics[metric].remaining > 0,
-    );
-    this.completionWeeks = {};
-    // 100 minimal height to accomodate enought space for the legend
-    const chartHeight = 50 + initiatives.length * 25;
-    return (
-      <div style={{ height: chartHeight }}>
-        <ResponsiveHeatMap
-          data={this.buildDataset(initiatives)}
-          keys={initiatives[0].weeks.map((w: any) => w.weekTxt)}
-          indexBy="initiative"
-          margin={{ top: 0, right: 30, bottom: 60, left: 300 }}
-          forceSquare={false}
-          axisTop={null}
-          axisRight={null}
-          cellBorderWidth={1}
-          axisBottom={{
-            orient: 'top',
-            tickSize: 5,
-            tickPadding: 5,
-            tickRotation: -90,
-            legend: '',
-            legendOffset: 36,
-          }}
-          axisLeft={{
-            orient: 'middle',
-            tickSize: 5,
-            tickPadding: 5,
-            tickRotation: 0,
-            legend: '',
-            legendPosition: 'middle',
-            legendOffset: -40,
-          }}
-          cellOpacity={1}
-          cellBorderColor={'#a4a3a5'}
-          labelTextColor={{ from: 'color', modifiers: [['darker', 1.8]] }}
-          cellShape={({
-            data,
-            value,
-            x,
-            y,
-            width,
-            height,
-            color,
-            opacity,
-            borderWidth,
-            borderColor,
-            enableLabel,
-            textColor,
-            onHover,
-            onLeave,
-            onClick,
-            theme,
-          }: any) => {
-            if (value === 0) {
-              return (
-                <g
-                  transform={`translate(${x}, ${y})`}
-                  style={{ cursor: 'pointer' }}
-                >
-                  <rect
-                    x={width * -0.5}
-                    y={height * -0.5}
-                    width={width}
-                    height={height}
-                    fill={''}
-                    fillOpacity={0}
-                    strokeWidth={borderWidth}
-                    stroke={borderColor}
-                    strokeOpacity={opacity}
-                  />
-                </g>
-              );
-            }
-            return (
-              <g
-                transform={`translate(${x}, ${y})`}
-                onMouseEnter={onHover}
-                onMouseMove={onHover}
-                onMouseLeave={onLeave}
-                onClick={e => {
-                  this.cellClick(data.yKey, data.xKey);
-                }}
-                style={{ cursor: 'pointer' }}
-              >
-                <rect
-                  x={width * -0.5}
-                  y={height * -0.5}
-                  width={width}
-                  height={height}
-                  fill={this.getCompletionColor(data, value)}
-                  fillOpacity={opacity}
-                  strokeWidth={borderWidth}
-                  stroke={borderColor}
-                  strokeOpacity={opacity}
-                />
-                {enableLabel && (
-                  <text
-                    dominantBaseline="central"
-                    textAnchor="middle"
-                    style={{
-                      ...theme.labels.text,
-                      fill: textColor,
-                    }}
-                    fillOpacity={opacity}
-                  >
-                    {value}
-                  </text>
-                )}
-              </g>
-            );
-          }}
-          animate={false}
-          motionStiffness={80}
-          motionDamping={9}
-          hoverTarget="row"
-          cellHoverOthersOpacity={0.1}
-        />
-      </div>
-    );
-  }
+	buildDataset = (initiatives: any) => {
+		const { defaultPoints } = this.props;
+		let metric = 'points';
+		if (!defaultPoints) {
+			metric = 'issues';
+		}
+
+		const dataset: IDatasetObj[] = [];
+		for (const initiative of initiatives) {
+			const initiativeData: IDatasetObj = {
+				initiative: getInitiativeTitle(initiative)
+			};
+			for (const week of initiative.weeks) {
+				initiativeData[week.weekTxt] = week[metric].count;
+			}
+			dataset.push(initiativeData);
+		}
+		return dataset;
+	};
+
+	render() {
+		const { roadmap, defaultPoints } = this.props;
+		let metric = 'points';
+		if (!defaultPoints) {
+			metric = 'issues';
+		}
+		//console.log(roadmap.futureCompletion);
+		const initiatives = roadmap.futureCompletion.filter((i: any) => i.metrics[metric].remaining > 0);
+		this.completionWeeks = {};
+		// 100 minimal height to accomodate enought space for the legend
+		const chartHeight = 50 + initiatives.length * 25;
+		return (
+			<div style={{ height: chartHeight }}>
+				<ResponsiveHeatMap
+					data={this.buildDataset(initiatives)}
+					keys={initiatives[0].weeks.map((w: any) => w.weekTxt)}
+					indexBy='initiative'
+					margin={{ top: 0, right: 30, bottom: 60, left: 300 }}
+					forceSquare={false}
+					axisTop={null}
+					axisRight={null}
+					cellBorderWidth={1}
+					axisBottom={{
+						orient: 'top',
+						tickSize: 5,
+						tickPadding: 5,
+						tickRotation: -90,
+						legend: '',
+						legendOffset: 36,
+						format: (v: any) => this.getDayWeek(v)
+					}}
+					axisLeft={{
+						orient: 'middle',
+						tickSize: 5,
+						tickPadding: 5,
+						tickRotation: 0,
+						legend: '',
+						legendPosition: 'middle',
+						legendOffset: -40,
+						onClick: (event: any, key: string) => {
+							this.clickLegend(key);
+						}
+					}}
+					cellOpacity={1}
+					cellBorderColor={'#a4a3a5'}
+					labelTextColor={{ from: 'color', modifiers: [ [ 'darker', 1.8 ] ] }}
+					cellShape={({
+						data,
+						value,
+						x,
+						y,
+						width,
+						height,
+						color,
+						opacity,
+						borderWidth,
+						borderColor,
+						enableLabel,
+						textColor,
+						onHover,
+						onLeave,
+						onClick,
+						theme
+					}: any) => {
+						if (value === 0) {
+							return (
+								<g transform={`translate(${x}, ${y})`} style={{ cursor: 'pointer' }}>
+									<rect
+										x={width * -0.5}
+										y={height * -0.5}
+										width={width}
+										height={height}
+										fill={''}
+										fillOpacity={0}
+										strokeWidth={borderWidth}
+										stroke={borderColor}
+										strokeOpacity={opacity}
+									/>
+								</g>
+							);
+						}
+						return (
+							<g
+								transform={`translate(${x}, ${y})`}
+								onMouseEnter={onHover}
+								onMouseMove={onHover}
+								onMouseLeave={onLeave}
+								onClick={(e) => {
+									this.cellClick(data.yKey, data.xKey);
+								}}
+								style={{ cursor: 'pointer' }}
+							>
+								<rect
+									x={width * -0.5}
+									y={height * -0.5}
+									width={width}
+									height={height}
+									fill={this.getCompletionColor(data, value)}
+									fillOpacity={opacity}
+									strokeWidth={borderWidth}
+									stroke={borderColor}
+									strokeOpacity={opacity}
+								/>
+								{enableLabel && (
+									<text
+										dominantBaseline='central'
+										textAnchor='middle'
+										style={{
+											...theme.labels.text,
+											fill: textColor
+										}}
+										fillOpacity={opacity}
+									>
+										{value}
+									</text>
+								)}
+							</g>
+						);
+					}}
+					animate={false}
+					motionStiffness={80}
+					motionDamping={9}
+					hoverTarget='row'
+					cellHoverOthersOpacity={0.1}
+				/>
+			</div>
+		);
+	}
 }
 
 export default withStyles(styles)(RoadmapFutureChart);
--- ui/src/views/roadmap/Completion/index.tsx
+++ ui/src/views/roadmap/Completion/index.tsx
@@ -11,89 +11,74 @@ import { connect } from 'react-redux';
 import { iRootState } from '../../../store';
 
 const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    root: {
-      padding: theme.spacing(3, 2),
-    },
-    smallText: {
-      fontSize: '0.8em',
-    },
-  }),
+	createStyles({
+		root: {
+			padding: theme.spacing(3, 2)
+		},
+		smallText: {
+			fontSize: '0.8em'
+		}
+	})
 );
 
 const mapState = (state: iRootState) => ({
-  defaultPoints: state.global.defaultPoints,
-  roadmap: state.roadmap.roadmap,
-  selectedTab: state.roadmap.selectedTab,
+	defaultPoints: state.global.defaultPoints,
+	roadmap: state.roadmap.roadmap,
+	selectedTab: state.roadmap.selectedTab
 });
 
 const mapDispatch = (dispatch: any) => ({
-  setDefaultPoints: dispatch.global.setDefaultPoints,
-  setGraphInitiative: dispatch.roadmap.setGraphInitiative,
-  updateGraph: dispatch.roadmap.updateGraph,
-  setOpenGraph: dispatch.roadmap.setOpenGraph,
+	setDefaultPoints: dispatch.global.setDefaultPoints,
+	setGraphInitiative: dispatch.roadmap.setGraphInitiative,
+	updateGraph: dispatch.roadmap.updateGraph,
+	setOpenGraph: dispatch.roadmap.setOpenGraph
 });
 
-type connectedProps = ReturnType<typeof mapState> &
-  ReturnType<typeof mapDispatch>;
+type connectedProps = ReturnType<typeof mapState> & ReturnType<typeof mapDispatch>;
 
 const Completion: FC<connectedProps> = ({
-  defaultPoints,
-  roadmap,
-  selectedTab,
-  setGraphInitiative,
-  updateGraph,
-  setOpenGraph,
+	defaultPoints,
+	roadmap,
+	selectedTab,
+	setGraphInitiative,
+	updateGraph,
+	setOpenGraph
 }) => {
-  const classes = useStyles();
-  let metric = 'points';
-  if (!defaultPoints) {
-    metric = 'issues';
-  }
+	const classes = useStyles();
+	let metric = 'points';
+	if (!defaultPoints) {
+		metric = 'issues';
+	}
 
-  if (Object.values(roadmap).length > 0 && selectedTab === 'completionchart') {
-    return (
-      <Grid
-        container
-        direction="column"
-        justify="flex-start"
-        alignItems="stretch"
-        spacing={1}
-      >
-        <Grid item xs={12}>
-          <Paper className={classes.root}>
-            <Typography variant="h5" component="h3">
-              Progress over the past weeks
-            </Typography>
-            <RoadmapCompletionChart
-              roadmap={roadmap}
-              defaultPoints={defaultPoints}
-            />
-            <br />
-            <Typography component="p" className={classes.smallText}>
-              <i>
-                Displays initiatives with completed {metric} over the period.
-              </i>
-            </Typography>
-          </Paper>
-        </Grid>
-        <Grid item xs={12}>
-          <InitiativeTable
-            initiatives={roadmap.initiatives}
-            jiraHost={roadmap.host}
-            setGraphInitiative={setGraphInitiative}
-            updateGraph={updateGraph}
-            setOpenGraph={setOpenGraph}
-          />
-        </Grid>
-      </Grid>
-    );
-  } else {
-    return null;
-  }
+	if (Object.values(roadmap).length > 0 && selectedTab === 'completionchart') {
+		return (
+			<Grid container direction='column' justify='flex-start' alignItems='stretch' spacing={1}>
+				<Grid item xs={12}>
+					<Paper className={classes.root}>
+						<Typography variant='h5' component='h3'>
+							Progress over the past weeks
+						</Typography>
+						<RoadmapCompletionChart roadmap={roadmap} defaultPoints={defaultPoints} />
+						<br />
+						<Typography component='p' className={classes.smallText}>
+							<i>Displays initiatives with completed {metric} over the period (week ending).</i>
+						</Typography>
+					</Paper>
+				</Grid>
+				<Grid item xs={12}>
+					<InitiativeTable
+						initiatives={roadmap.initiatives}
+						jiraHost={roadmap.host}
+						setGraphInitiative={setGraphInitiative}
+						updateGraph={updateGraph}
+						setOpenGraph={setOpenGraph}
+					/>
+				</Grid>
+			</Grid>
+		);
+	} else {
+		return null;
+	}
 };
 
-export default connect(
-  mapState,
-  mapDispatch,
-)(Completion);
+export default connect(mapState, mapDispatch)(Completion);
