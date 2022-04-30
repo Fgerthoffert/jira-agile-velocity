4d4bc29fca3c8e1e82c3a2d892797a2f2781a273
Francois Gerthoffert <f.gerthoffert@gmail.com>
2022-04-30 09:00:33 -0400

    Fixed env config

--- ui/env-config.js
+++
@@ -1,8 +0,0 @@
-window._env_ = {
-  SKIP_PREFLIGHT_CHECK: "true",
-  API_URL: "http://127.0.0.1:3001",
-  AUTH0_DISABLED: "true",
-  AUTH0_DOMAIN: "agile-jav.auth0.com",
-  AUTH0_CLIENT_ID: "sGJDsqzc0VhKgoutaoyLWjyWK9kAwE2f",
-  AUTH0_AUDIENCE: "http://localhost:3001",
-}
